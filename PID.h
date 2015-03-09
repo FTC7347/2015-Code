cb86b2335a3cc955765ce4fc99252ccafe0914c8
unknown <flewulis@LCHSMLAB3-18.lcs.libertycommon.org>
2015-03-09 13:53:01 -0600

    Created PID

--- PID.h
+++ PID.h
@@ -3,15 +3,42 @@
 
 struct PIDController {
 	float kP, kI, kD, iAccumulator, setPoint;
-
+	bool cont;
+	int lastTime;
+	float maxOutput, maxInput, minOutput, minInput;
+	float prevError;
 }
 
 float PIDUpdate(float input, struct PIDController *ctr) {
 	float output = 0;
-	output += ctr->kP * (input - ctr->setPoint);
-	//output += ctr->kI * ();
+	float error = ctr->setPoint - input;
+	if(ctr->cont) {
+		if (fabs(error)>(ctr->maxInput - ctr->minInput)/2) {
+			if (error > 0) {
+				error = error - ctr->maxInput + ctr->minInput;
+			} else {
+				error = error + ctr->maxInput - ctr->minInput;
+			}
+		}
+	}
+	if (ctr->kI != 0) {
+		float potentialIGain = (ctr->iAccumulator + error) * ctr->kI;
+		if (potentialIGain < ctr->maxOutput) {
+			if (potentialIGain > ctr->minOutput) {
+				ctr->iAccumulator += error;
+			} else {
+				ctr->iAccumulator = ctr->minOutput / ctr->kI;
+			}
+		} else {
+			ctr->iAccumulator = ctr->maxOutput / ctr->kI;
+		}
+	}
+	result = ctr->kP * error + ctr->kI * ctr->iAccumulator + ctr->kD * (error - ctr->prevError);
+	ctr->prevError = error;
+	if (result > ctr->maxOutput) result = ctr->maxOutput;
+	else if (result < ctr->minOutput) result = ctr->minOutput;
 
-
+	return result;
 }
 
 #endif
